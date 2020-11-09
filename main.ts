65642e1e880fc1bc11973c008c7cdd27ee320014
mario1278 <73850760+mario1278@users.noreply.github.com>
2020-11-09 16:05:05 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,8 +1,21 @@
 input.onButtonPressed(Button.A, function () {
-    for (let index = 0; index <= 9; index++) {
+    for (let index = 0; index <= 1999; index++) {
         basic.showNumber(RUTAS[index])
     }
 })
+input.onButtonPressed(Button.B, function () {
+    let index = 0
+    VALOR_MIN = RUTAS[index]
+    for (let index = 0; index <= 1999; index++) {
+        if (VALOR_MIN > RUTAS[index]) {
+            VALOR_MX = RUTAS[index]
+        }
+    }
+    basic.showString("EL TIEMPO EN ESTA RUTA ES")
+    basic.showNumber(VALOR_MIN)
+})
+let VALOR_MX = 0
+let VALOR_MIN = 0
 let RUTAS: number[] = []
 let vramdom = 25
 RUTAS = [2000]
